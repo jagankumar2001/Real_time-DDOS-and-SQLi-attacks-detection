81776870416ce7cf07a7d57e7af50bb8b06de4de
jagankumar2001 <cjagankumar2001@gmail.com>
2025-07-29 21:08:11 +0530

    Changes commited to this

--- web_ch1.py
+++ web_ch1.py
@@ -122,4 +122,5 @@ if __name__ == "__main__":
 
     # app.run(debug=True,port=5001)
     # app.run(debug=False, port=5001, use_reloader=False)
-    app.run(host='127.0.0.1', port=5001, debug=False, use_reloader=False)
+    #app.run(host='127.0.0.1', port=5001, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5001, debug=False, use_reloader=False)
